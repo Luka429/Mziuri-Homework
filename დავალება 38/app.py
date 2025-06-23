7322552504507a7e413643aa3dd79e25503a629d
Luka429 <Lukadodde@gmail.com>
2025-06-23 17:54:19 +0400

    Updated a file

--- დავალება 38/app.py
+++ დავალება 38/app.py
@@ -3,31 +3,64 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.confid['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(100))
-    password = db.Columt(db.String(30))
+    password = db.Column(db.String(30))
 
-class post(db.model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     description = db.Column(db.String(150))
-    username = db.Columt(db.string(100), db.ForeignKey('User.username'))
+    username = db.Column(db.String(100), db.ForeignKey('User.username'))
 
 @app.route("/add_post", methods =["GET", "POST"])
 def add_post():
     if request.method == "POST":
         description = request.form["description"]
         username = request.form["username"]
-        new_post = post(description=description, username=username)
+        new_post = Post(description=description, username=username)
     
     for User_username in User:
         if User_username:
             existing_username = User.query.filter_by(username=User_username).first()
             if existing_username:
-                post.username.append(existing_username)
+                Post.username.append(existing_username)
+                return "successfully posted"
             else:
+                return redirect(url_for('login'))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return("username is already taken choose another one")
+            
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
         
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            return redirect(url_for('add_post')) 
+
+        return redirect(url_for('login'))
+
+    
